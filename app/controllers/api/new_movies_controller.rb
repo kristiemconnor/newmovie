008537f769cf65acace51288589c43e826e2222c
kristie <kristiemconnor@gmail.com>
2019-09-22 22:17:27 -0400

    adds destroy, update, create action to movie controller

--- app/controllers/api/new_movies_controller.rb
+++ app/controllers/api/new_movies_controller.rb
@@ -10,5 +10,40 @@ class Api::NewMoviesController < ApplicationController
     render 'show.json.jb'
   end
 
+  
+  def create
+    @movie = Movie.new(
+      title: params[:title],
+      year: params[:year],
+      plot: params[:plot],
+      director: params[:director]
+      )
+    if @movie.save
+      render 'show.json.jb'
+    else
+      render json:{Errors: @movie.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.title = params[:title] || @movie.title
+    @movie.year = params[:year] || @movie.year
+    @movie.plot = params[:plot] || @movie.plot
+    @movie.director = params[:director] || @movie.director
+    @movie.english = params[:english] || @movie.english
+    
+    if @movie.save
+      render 'show.json.jb'
+    else
+      render json:{Errors: @movie.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    render json:[movie: "Gone forever"]
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   namespace :api do
     get "/movies" => "new_movies#index"
     get "/movies/:id" => "new_movies#show"
+    post "/movies" => "new_movies#create"
+    patch "/movies/:id" => "new_movies#update"
+    delete "/movies/:id" => "new_movies#destroy"
+
     post "/users" => "users#create"
     post "/sessions" => "sessions#create"
   end
