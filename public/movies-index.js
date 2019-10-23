e6211df87f606c32f4e01b48a62ea6605ed4fac2
kristie <kristiemconnor@gmail.com>
2019-10-23 02:18:31 -0400

    updates movies-index.html

--- public/movies-index.js
+++ public/movies-index.js
@@ -2,3 +2,4 @@ axios.get("http://localhost:3000/api/movies").then(function(response) {
   var movies = response.data;
   console.log(movies);
 });
+x;
---
+++ public/posts.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="posts-index">
+    <h1>All Posts</h1>
+
+    <div v-for="post in posts">
+      <h2>Title: {{ post.title }}</h2>
+
+      <div v-if="post === currentPost">
+        <p>Post: {{ post.title }}</p>
+        <p>Date: {{ post.date }}</p>
+        <p>{{ post.body }}</p>
+        <h4>Edit Recipe</h4>
+        <div>
+          Title:
+          <input type="text" v-model="recipe.title" />
+          <br />
+          Ingredients:
+          <input type="text" v-model="recipe.ingredients" />
+          <br />
+          Directions:
+          <input type="text" v-model="post.d" />
+          <br />
+
+          <button v-on:click="updatePost(post)">Update</button>
+          <br />
+          <button v-on:click="destroyPost(post)">Destroy</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
